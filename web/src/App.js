969c93f1231d028ae9cf6b2b3d34f2d4b24f9a81
qu0est <qu0est@gmail.com>
2020-05-28 11:38:35 -0400

    add files

---
+++ web/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { 
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache 
+} from '@apollo/client';
+
+import { setContext } from 'apollo-link-context';
+import GlobalStyle from '/components/GlobalStyle';
+import Pages from '/pages';
+
+//* configure our API URI & cache
+const uri = process.env.API_URI;
+const httpLink = createHttpLink({ uri });
+const cache = new InMemoryCache();
+
+//! check for a token and return the headers to the context
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || ''
+    }
+  };
+});
+
+//TODO check for a local token
+const data = {
+  isLoggedIn: !!localStorage.getItem('token');
+};
+
+//* write the cache data on initial load
+cache.writeData({ data });
+//! write the cache data after the cache is reset
+client.onResetStore.(() => cache.writeData({ data }));
+
+//? configure Apollo Client
+const client = new ApolloClient ({
+  link: authLink.concat(httpLink),
+  cache,
+  resolvers: {},
+  connectToDevTools: true
+});
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <GlobalStyle />
+      <Pages />
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ web/src/components/Header.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import styled from 'styled-components';
+import ButtonAsLink from './ButtonAsLink';
+
+const HeaderBar = styled.header`
+    width: 100%;
+    padding: 0.5em 1em;
+    display: flex;
+    height: 64px;
+    position: fixed;
+    align-items: center;
+    background-color: #fff;
+    box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.25);
+    z-index: 1;
+`;
+
+const LogoText = styled.h1`
+    margin: 0;
+    padding: 0;
+    display: inline;
+`;
+
+const Header = (props) => {
+    //!query hook for user logged-in state,
+    //!including the client for referencing the Apollo store
+    const { data, client } = useQuery(IS_LOGGED_IN);
+    return (
+        <HeaderBar>
+            <img src={logo} alt="Notedly Logo" height="40" />
+           <LogoText>Notedly</LogoText>
+           {/*If logged in display a logout link, else display sign-in options*/}
+           <UserState>
+               {data.isLoggedIn ? (
+                   <ButtonAsLink
+                   onClick={() =>{
+                       //* remove the token
+                       localStorage.removeItem('token');
+                       //! clear the application's cache
+                       client.resetStore();
+                       //? update local state
+                       client.writeData({data: { isLoggedIn: false} });
+                       //* redirect the user to the home page
+                       props.history.push('/');
+                   }}
+                   >
+                      Logout
+                   </ButtonAsLink>
+               ) : (
+                   <p>
+                       <Link to={'/signin'}>Sign In</Link> or{' '}
+                       <Link to={'/signup'}>Sign Up</Link>
+                   </p>
+               )}
+           </UserState>
+        </HeaderBar>
+    );
+};
+//TODO we wrap our component in the withRouter higher-order component
+export default withRouter(Header);
